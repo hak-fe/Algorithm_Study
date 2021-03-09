c883b2f736be1583b18c7d52755f56213401ca14
hak-fe <71654651+hak-fe@users.noreply.github.com>
2021-03-09 18:28:50 +0900

    Add files via upload

---
+++ C/최댓값 구하기.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+
+	int arr[9];
+	int max = 0, cnt = 0;
+	for (int i = 0; i < 9; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+
+	for (int i = 0; i < 9; i++)
+	{
+		if (arr[i] > max) {
+		
+			max = arr[i];
+			cnt = i + 1;
+		}
+	}
+	printf("%d\n%d", max, cnt);
+	return 0;
+}
