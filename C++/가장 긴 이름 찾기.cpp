efd831cb94edf1bcb6fc1551a762191f2f6247b1
hak-fe <71654651+hak-fe@users.noreply.github.com>
2021-03-13 13:10:49 +0900

    5명의 이름 중 가장 긴 이름을 출력하는 프로그램

---
+++ C++/가장 긴 이름 찾기.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(void) {
+
+	char name[100];
+	char LongestName[100];
+	int name_len[5];
+	for (int i = 0; i < 5; i++)
+	{
+		
+		cin.getline(name, 100, ';');
+		if (i == 0) {
+			strcpy_s(LongestName, name);
+		}
+		cout << i + 1 << " : " << name << endl;
+		name_len[i] = strlen(name);
+		if (i > 0) {
+			if (name_len[i] > name_len[i - 1]) {
+				strcpy_s(LongestName, name);
+			}
+		}
+	}
+	cout << "가장 긴 이름은 " << LongestName <<endl;
+}               
