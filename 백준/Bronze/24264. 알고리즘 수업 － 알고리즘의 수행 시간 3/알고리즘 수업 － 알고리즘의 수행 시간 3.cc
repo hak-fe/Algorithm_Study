670522d5b9f24ff9c6058b80985cfef459bedf0a
hak-fe <71654651+hak-fe@users.noreply.github.com>
2024-11-25 16:28:24 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	
+	long long n;
+	cin >> n;
+	cout << n * n << '\n' << 2;
+	return 0;
+}
