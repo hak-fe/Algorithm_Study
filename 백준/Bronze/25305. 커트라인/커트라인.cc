f6d8fe84523c32beb8d74ac32274a8bb37037d09
hak-fe <71654651+hak-fe@users.noreply.github.com>
2024-11-26 16:57:16 +0900

    [Bronze II] Title: 커트라인, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n, k, x;
+	vector<int>v;
+	cin >> n >> k;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> x;
+		v.push_back(x);
+	}
+	sort(v.begin(), v.end());
+	cout << v[(n - 1) - k + 1];
+	return 0;
+}
