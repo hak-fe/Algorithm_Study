cc9ff4c556e7fd2c9cf6d88d3497e16fa59fab30
hak-fe <71654651+hak-fe@users.noreply.github.com>
2024-12-01 14:34:14 +0900

    [Silver II] Title: 베르트랑 공준, Time: 12 ms, Memory: 2984 KB -BaekjoonHub

---
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+using namespace std;
+typedef long long int ll;
+int prime[246913];
+ll isPrime(ll num);
+void getPrime();
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	ll n, temp, cnt;
+	fill(prime, prime + 246913, 1);
+	prime[1] = 0;
+	getPrime();
+	while (1) {
+		cnt = 0;
+		cin >> n;
+		if (n == 0)
+			return 0;
+		for (ll i = n+1; i <= 2 * n; i++)
+			if (prime[i] == 1)
+				cnt++;
+		cout << cnt << '\n';
+	}
+	return 0;
+}
+void getPrime() {
+	for (int i = 2; i * i <= 246912; i++)
+	{
+		for (int j = i * i; j <= 246912; j += i)
+		{
+			if (prime[j])
+				prime[j] = 0;
+		}
+	}
+}
+ll isPrime(ll num) {
+	while (1) {
+		int isPrime = 1;
+		for (ll i = 2; i * i <= num; i++)
+		{
+			if (num % i == 0) {
+				isPrime = 0;
+				break;
+			}
+		}
+		if (isPrime)
+			return num;
+		num++;
+	}
+}
