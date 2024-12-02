60579e3b6587e320c8928af503e202cc600cace1
hak-fe <71654651+hak-fe@users.noreply.github.com>
2024-12-02 17:19:56 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.cc
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.cc
@@ -8,27 +8,6 @@
 
 using namespace std;
 typedef long long int ll;
-/*
-while - 대기열과 왼쪽 공간이 전부 빌 때 까지
-	if - 대기열과 왼쪽공간 둘 다 학생이 존재하는 경우
-		1. 왼쪽공간 확인하기
-			1-1 order와 번호표가 같으면 보내기, order++
-			1-2 order와 번호표가 다르면 동작x
-		2. 대기열 확인하기
-			2-1 order와 번호표가 같으면 보내기, order++
-			2-2 order와 번호표가 다르면 한 명을 왼쪽 공간으로 보내기
-
-	 else if - 대기열만 존재하는 경우
-		1. order와 번호표가 같은 경우
-			보내고 order++
-		2. 다른 경우 왼쪽 공간으로 한 명 보내기
- 
-	 else - 왼쪽공간만 존재하는 경우
-		1. order와 번호표가 같은경우
-			보내고 order++
-		2. order와 번호표가 다른 경우
-			Sad 출력 후 프로그램 종료
-*/
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
