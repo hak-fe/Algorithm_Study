038c854c13adbe6b9feb0fed2f77bd86906420c3
hak-fe <71654651+hak-fe@users.noreply.github.com>
2024-12-16 18:12:27 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 136 ms, Memory: 13864 KB -BaekjoonHub

---
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+#include <map>
+using namespace std;
+typedef long long int ll;
+map<string, int> mmap;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	string word;
+	int n, m;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> word;
+		if (word.length() >= m) {
+			if (mmap.find(word) == mmap.end()) {
+				mmap[word] = 1;
+			}
+			else
+				mmap[word]++;
+		}
+	}
+
+
+	vector<pair<string, int>> sorted(mmap.begin(), mmap.end());
+	sort(sorted.begin(), sorted.end(), [](const pair<string, int>& a, const pair<string, int>& b) {
+		// 우선순위 1: 빈도수 많은 게 앞으로
+		if (a.second != b.second)
+			return a.second > b.second;
+
+		// 우선순위 2: 키의 길이 기준 내림차순
+		if (a.first.length() != b.first.length())
+			return a.first.length() > b.first.length();
+
+		// 우선순위 3: 알파벳 순서로 오름차순
+		return a.first < b.first;
+		});
+
+	for (auto it : sorted)
+		cout << it.first << '\n';
+
+	return 0;
+}
