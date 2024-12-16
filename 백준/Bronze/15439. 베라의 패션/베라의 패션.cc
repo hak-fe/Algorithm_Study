22d545ca8c8a33e6e447ca9109e620125177590c
hak-fe <71654651+hak-fe@users.noreply.github.com>
2024-12-16 17:03:03 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+using namespace std;
+typedef long long int ll;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n;
+	cin >> n;
+	cout << n * (n - 1);
+	return 0;
+}
