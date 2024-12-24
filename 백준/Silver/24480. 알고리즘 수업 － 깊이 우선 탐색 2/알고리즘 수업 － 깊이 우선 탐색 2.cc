c39d90e20c1a9e755dc838ae96320786436b0842
hak-fe <71654651+hak-fe@users.noreply.github.com>
2024-12-24 18:13:33 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 76 ms, Memory: 10928 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+#include <map>
+using namespace std;
+typedef long long int ll;
+int visited[100001]; // 방문확인용
+void dfs(int start);
+int cnt = 1;
+vector<vector<int>>graph;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n, m, r, s, e;
+	cin >> n >> m >> r;
+	graph.resize(n+1);
+	for (int i = 0; i < m; i++)
+	{
+		cin >> s >> e;
+		graph[s].push_back(e);
+		graph[e].push_back(s);
+	}
+	for (int i = 1; i <= n; i++)
+		sort(graph[i].begin(), graph[i].end(), greater<int>());
+	dfs(r);
+	for (int i = 1; i <= n; i++)
+		cout << visited[i] << '\n';
+	return 0;
+}
+void dfs(int start) {
+	visited[start] = cnt++;
+	for (int i = 0; i < graph[start].size(); i++)
+		if (visited[graph[start][i]] == 0)
+			dfs(graph[start][i]);
+}
