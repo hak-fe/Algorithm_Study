3ce4fe785fa44f7c5bfa5fe31cd2524cb763dc56
Hak Cheol Shin <71654651+hak-fe@users.noreply.github.com>
2025-08-07 09:05:05 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+
+#define DEBUG 0
+#define endl '\n'
+#define FASTIO cin.tie(NULL), ios_base::sync_with_stdio(false)
+#define DIV 10007
+#define MAX 2001
+using namespace std;
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+
+int main() {
+	FASTIO;
+	priority_queue<int, vector<int>>p;
+	int n, t;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> t;
+		if (t == 0) {
+			if (p.empty())
+				cout << 0 << endl;
+			else {
+				cout << p.top() << endl;
+				p.pop();
+			}
+		}
+		else
+			p.push(t);
+	}
+	
+	return 0;
+}
