fd52de45aae452dcdcbec1fe3057365f2ea7a69a
Hak Cheol Shin <71654651+hak-fe@users.noreply.github.com>
2025-08-13 12:12:21 +0900

    [Silver I] Title: 절댓값 힙, Time: 12 ms, Memory: 2532 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+
+#define DEBUG 0
+#define endl '\n'
+#define FASTIO cin.tie(NULL), ios_base::sync_with_stdio(false)
+#define DIV 10007
+#define MAX 2001
+
+using namespace std;
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+int dz[2] = { 1, -1 };
+
+struct comp
+{
+	bool operator()(int a, int b) {
+		if (abs(a) == abs(b))
+			return a > b;
+		return abs(a) > abs(b);
+	}
+};
+
+int main() {
+	FASTIO;
+	priority_queue<int, vector<int>,comp>pq;
+	int n;
+	cin >> n;
+	for (int i = 0, temp; i < n; i++)
+	{
+		cin >> temp;
+		if (!temp) {
+			if (pq.empty())
+				cout << 0 << endl;
+			else {
+				cout << pq.top() << endl;
+				pq.pop();
+			}
+		}
+		else
+			pq.push(temp);
+	}
+}
