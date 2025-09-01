af790ef087dffb5256a6caaae24a262724e812d5
Hak Cheol Shin <71654651+hak-fe@users.noreply.github.com>
2025-09-01 22:13:35 +0900

    [Bronze V] Title: 제리와 톰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <set>
+#include <stdlib.h>
+
+#define DEBUG 0
+#define endl '\n'
+#define FASTIO cin.tie(NULL), ios_base::sync_with_stdio(false)
+#define DIV 10000
+#define MAX 100001
+#define INF 2100000000
+using namespace std;
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+int dz[2] = { 1, -1 };
+
+vector<vector<int>>v;
+vector<pair<int, int>> bbq;
+int main() {
+	FASTIO;
+
+	int n, m;
+	cin >> n >> m;
+    cout << m - n <<' ' << m;
+
+	return 0;
+}
