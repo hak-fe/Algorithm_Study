fcd989b43e00ced3531591aaff1ce2905525c97d
Hak Cheol Shin <71654651+hak-fe@users.noreply.github.com>
2025-10-06 22:16:25 +0900

    [Bronze IV] Title: 특별한 가지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31668. 특별한 가지/특별한 가지.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <math.h>
+#include <stdlib.h>
+
+#define DEBUG 0
+#define endl '\n'
+#define FASTIO cin.tie(NULL), ios_base::sync_with_stdio(false)
+#define DIV 10000
+#define MAX 100001
+#define INF 2100000000
+using namespace std;
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+int dz[2] = { 1, -1 };
+vector<string>v;
+int main() {
+	FASTIO;
+	int n, m ,k;
+	cin >> n >> m >> k;
+	cout << m / n * k;
+	return 0;
+}
