3ff7065e54a3c5a574033b29cd1bf43953077160
Hak Cheol Shin <71654651+hak-fe@users.noreply.github.com>
2025-10-06 22:02:34 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <math.h>
+#include <stdlib.h>
+
+#define DEBUG 0
+#define endl '\n'
+#define FASTIO cin.tie(NULL), ios_base::sync_with_stdio(false)
+#define DIV 10000
+#define MAX 100001
+#define INF 2100000000
+using namespace std;
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+int dz[2] = { 1, -1 };
+
+int main() {
+	FASTIO;
+	cout << "2025\n10\n06";
+	return 0;
+}
